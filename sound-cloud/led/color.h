8f3c6b11beb791eaa99f620c92ddda458db7c26e
Mikayla Fischler <micron1314@gmail.com>
2019-04-22 14:37:14 -0400

    color definitions

---
+++ sound-cloud/led/color.h
@@ -0,0 +1,41 @@
+#ifndef CLOUD_COLOR_HPP_
+#define CLOUD_COLOR_HPP_
+
+/* ~~~ color definitions ~~~ */
+
+// color category						// (red, green, blue)
+
+// basic colors
+#define COLOR_OFF 0 					// (0, 0, 0)
+
+#define COLOR_BLACK 0 					// (0, 0, 0)
+#define COLOR_WHITE 16777215 			// (255, 255, 255)
+
+#define COLOR_RED 16711680 				// (255, 0, 0)
+#define COLOR_MAROON 16717083 			// (255, 21, 27)
+#define COLOR_ORANGE 16724480 			// (255, 50, 0)
+#define COLOR_YELLOW 16750080			// (255, 150, 0)
+#define COLOR_GREEN 65280 				// (0, 255, 0)
+#define COLOR_BLUE 255 					// (0, 0, 255)
+#define COLOR_INDIGO 6553855			// (100, 0, 255)
+#define COLOR_PURPLE 11469055 			// (175, 0, 255)
+#define COLOR_MAGENTA 16711935 			// (255, 0, 255)
+#define COLOR_PINK 16711860				// (255, 0, 180)
+
+// specific colors
+
+// whites
+#define COLOR_DIM_WHITE 6579300			// (100, 100, 100)
+#define COLOR_DIM_OFF_WHITE 4926745 	// (75, 45, 25)
+#define COLOR_LIGHT_SILVER 11120823 	// (169, 176, 183)
+
+// greens
+#define COLOR_SEA_GREEN 65400			// (0, 255, 120)
+#define COLOR_LIGHT_BLUE_GREEN 65480 	// (0, 255, 200)
+
+// blues
+#define COLOR_BABY_BLUE 6016762			// (91, 206, 250)
+#define COLOR_LIGHT_BLUE 65535			// (15, 15, 255)
+#define COLOR_RAINY_BLUE 987135			// (0, 255, 255)
+
+#endif
