7a05e3c4741e23f59f27c076c23065efefc847cb
Mikayla Fischler <micron1314@gmail.com>
2019-04-22 14:40:11 -0400

    some testing

--- sound-cloud/sound-cloud.ino
+++ sound-cloud/sound-cloud.ino
@@ -3,8 +3,13 @@
 // Since the arduino IDE struggles with C files nested in folders, 
 // we will include them only if arduino is being compiled here
 #include "audio/Audio.cpp"
+
 #include "io/io.c"
+
 #include "led/LEDCloudMatrix.cpp"
+
+#include "led/Animations.cpp"
+#include "led/Animations.Standby.cpp"
 #endif
 
 #include <Adafruit_NeoPixel.h>
@@ -38,8 +43,47 @@ void setup() {
 
 	// create our cloud matrix
 	c_matrix = new LEDCloudMatrix(&row_1_2_led_strip, &row_3_4_led_strip, &top_led_strip);
+
+	// set up matrix segmentation
+	led_segment_t* level_0 = c_matrix->createSegment(LEDCloudMatrix::ROW_1_2, 0, 23);
+	led_segment_t* level_1 = c_matrix->createSegment(LEDCloudMatrix::ROW_1_2, 23, 23);
+	led_segment_t* level_2 = c_matrix->createSegment(LEDCloudMatrix::ROW_3_4, 0, 21);
+	led_segment_t* level_3 = c_matrix->createSegment(LEDCloudMatrix::ROW_3_4, 21, 19);
+
+	led_segment_t* top_0  = c_matrix->createSegment(LEDCloudMatrix::TOP, 0, 11);
+	led_segment_t* top_0_ = c_matrix->createSegment(LEDCloudMatrix::ROW_3_4, 40, 1);
+	led_segment_t* top_1  = c_matrix->createSegment(LEDCloudMatrix::TOP, 11, 12);
+	led_segment_t* top_1_ = c_matrix->createSegment(LEDCloudMatrix::ROW_3_4, 41, 1);
+	led_segment_t* top_2  = c_matrix->createSegment(LEDCloudMatrix::TOP, 23, 12);
+	led_segment_t* top_2_ = c_matrix->createSegment(LEDCloudMatrix::ROW_3_4, 42, 1);
+	led_segment_t* top_3  = c_matrix->createSegment(LEDCloudMatrix::TOP, 35, 13);
+	led_segment_t* top_3_ = c_matrix->createSegment(LEDCloudMatrix::TOP, 48, 12);
+
+	Serial.println("MEMES");
+	c_matrix->mapSegmentToLevel(level_0, 0);
+	Serial.println("MEMES");
+	c_matrix->mapSegmentToLevel(level_1, 1);
+	Serial.println("MEMES");
+	c_matrix->mapSegmentToLevel(level_2, 2);
+	Serial.println("MEMES");
+	c_matrix->mapSegmentToLevel(level_3, 3);
+	Serial.println("MEMES");
+
+	// c_matrix->mapSegmentToTopMatrix()
+
+
+	// setup animations system
+	Animations::linkAudio(audio);
+	Animations::linkCloudMatrix(c_matrix);
 }
 
 void loop() {
-
+	for (int i = 0; i < c_matrix->levelLength(0); i++) {
+		c_matrix->setPixelColorByLevel(0, i, COLOR_DIM_OFF_WHITE);
+		Serial.println(i);
+	}
+	c_matrix->showLevel(0);
+	delay(1000);
+	// Animations::Standby::solidSoftWhite();
+	// Serial.println("HEARTBEAT");
 }
