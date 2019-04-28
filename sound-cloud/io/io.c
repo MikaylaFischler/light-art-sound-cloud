498d0a1ce6bb0873670e5c52b1f5a5707f98b1ef
Mikayla Fischler <micron1314@gmail.com>
2019-04-28 01:04:41 -0400

    added status LED implementation and some comments in the main file

--- sound-cloud/io/io.c
+++ sound-cloud/io/io.c
@@ -4,7 +4,7 @@ uint8_t led_mode = 0;
 uint8_t hysteresis = 0x00;
 
 /**
- * @brief 
+ * @brief Initialize the mode button control
  * 
  */
 void io_init_mode_control(void) {
@@ -32,15 +32,56 @@ void io_change_mode(void) {
 }
 
 /**
- * @brief 
+ * @brief Get the current mode to be running in
  * 
- * @return uint8_t 
+ * @return uint8_t The mode
  */
 uint8_t io_get_mode(void) { return led_mode; }
 
 /**
- * @brief 
+ * @brief Initialize status LED to off
  * 
- * @return int 
  */
-int io_get_brightness(void) { return analogRead(LED_BRIGHTNESS_PIN); }
+void io_init_stat_led(void) {
+	// default to off
+	analogWrite(S_LED_R, 255);
+	analogWrite(S_LED_G, 255);
+	analogWrite(S_LED_B, 255);
+}
+
+/**
+ * @brief Set the Common Anode RGB status LED color
+ * 
+ * @param r Red component (0 - 255)
+ * @param g Green component (0 - 255)
+ * @param b Blue component (0 - 255)
+ */
+void __io_write_status_led(uint8_t r, uint8_t g, uint8_t b) {
+	analogWrite(S_LED_R, 255 - r);
+	analogWrite(S_LED_G, 255 - g);
+	analogWrite(S_LED_B, 255 - b);
+}
+
+/**
+ * @brief Set the status LED to a yellowish color to indicate system is starting up
+ * 
+ */
+void io_led_write_starting(void) {
+	__io_write_status_led(200, 150, 0);
+}
+
+/**
+ * @brief Set the status LED to green to indicate system is running
+ * 
+ */
+void io_led_write_active(void) {
+	__io_write_status_led(0, 200, 50);
+}
+
+/**
+ * @brief Set the status LED to blue to indicate state is changing
+ * 
+ */
+void io_led_write_changing(void) {
+	__io_write_status_led(0, 0, 150);
+}
